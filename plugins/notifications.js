922c5d673453e09d4e0f64dd762b58f46c84a98b
Sebastien Kempf <56154464+skmpf@users.noreply.github.com>
2026-04-06 21:52:54 +0900

    chore: remove opencode

--- plugins/notifications.js
+++
@@ -1,9 +0,0 @@
-export const NotificationPlugin = async () => {
-  return {
-    event: async ({ event, $ }) => {
-      if (event.type === "session.idle") {
-        await $`afplay /System/Library/Sounds/Purr.aiff`;
-      }
-    },
-  };
-};
